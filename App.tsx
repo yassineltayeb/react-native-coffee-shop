cfe20b3f710a29ec419feaeea2adc8ee715792d1
Yassin Eltayeb <yassineltayeb@live.com>
2024-06-11 14:48:04 +0400

    added fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,36 @@
 import Navigation from "components/common/Navigation";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
+import * as Font from 'expo-font';
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        'app_icons': require('./assets/fonts/app_icons.ttf'),
+        'Poppins-Black': require('./assets/fonts/Poppins-Black.ttf'),
+        'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+        'Poppins-ExtraBold': require('./assets/fonts/Poppins-ExtraBold.ttf'),
+        'Poppins-ExtraLight': require('./assets/fonts/Poppins-ExtraLight.ttf'),
+        'Poppins-Light': require('./assets/fonts/Poppins-Light.ttf'),
+        'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
+        'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+        'Poppins-SemiBold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+        'Poppins-Thin': require('./assets/fonts/Poppins-Thin.ttf'),
+      });
+      setFontsLoaded(true);
+    }
+
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <StatusBar style="dark" />
